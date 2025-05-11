2289dad73e5dd35a4a328fedf5a8ad2d08e8b844
Alex Cuffe <contxpr@outlook.com>
2025-05-11 16:50:19 -0300

    Implementação Função Cadastrar Produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -3,36 +3,44 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
-import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
-    
-    Connection conn;
+
+    conectaDAO conn = new conectaDAO();
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public int cadastrarProduto(ProdutosDTO produto) {
+
+        int status;
+
+        try {
+            prep = conn.connectDB().prepareStatement("INSERT INTO produtos(nome, valor, status) VALUES (?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3,produto.getStatus());
+            status = prep.executeUpdate();
+            return status;
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao Conectar: " + ex.getMessage());
+            return ex.getErrorCode();
+        } catch (NullPointerException np) {
+            JOptionPane.showMessageDialog(null, np.getMessage());
+            return 0;
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
