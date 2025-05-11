3cbac243275beec9d5ba88439083ca84ed3f4202
Alex Cuffe <contxpr@outlook.com>
2025-05-11 16:21:56 -0300

    Ajuste Conexão

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,30 +4,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
 public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
+
+    Connection conn = null;
+    private final String url = "jdbc:mysql://localhost:3306/uc11";
+    private final String usuario = "root";
+    private final String senha = "152152";
+
+    public Connection connectDB() {
+
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, usuario, senha);
+            JOptionPane.showMessageDialog(null, "Conexão Realizada com Sucesso");
+            return conn;
+
+        } catch (ClassNotFoundException | SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            return null;
         }
-        return conn;
+
     }
-    
+
 }
